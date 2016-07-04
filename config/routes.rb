472d8406faed2c21baf279c370450aa7638790ba
Alexander-Andrade <veimar.94@gmail.com>
2016-07-04 14:54:54 +0300

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'account_activations/edit'
+
   get 'sessions/new'
 
   # You can have the root of your site routed with "root"
@@ -11,6 +13,7 @@ Rails.application.routes.draw do
   post 'login'   =>    'sessions#create'
   delete 'logout'=>    'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
