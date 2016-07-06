55691440ffecfae52bac26ebc36a7e0a85e22288
Alexander-Andrade <veimar.94@gmail.com>
2016-07-06 13:43:11 +0300

    there is user micropost

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
 
   def setup
     @user = users(:michael)
@@ -9,7 +10,14 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   test "profile display" do
     get user_path(@user)
     assert_template 'users/show'
-    assert_select 'title', "#{@user.name} | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
   end
 
 end
