5c61e5d9b6c3a192baea966b137fe6cdd0a7e6f2
Alexander-Andrade <veimar.94@gmail.com>
2016-07-08 12:14:59 +0300

    add fonts to custom css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   #Returns the full titile on a per-page basis.
 
   def full_title(page_title= '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Ruby Social Network"
     (page_title.empty?) ? base_title : "#{page_title} | #{base_title}"
   end
 end
