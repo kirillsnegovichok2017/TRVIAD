b2ab5f26d0d509de487df8742964c7fe2515e240
Alexander-Andrade <veimar.94@gmail.com>
2016-07-08 17:14:22 +0300

    promo account

---
+++ app/assets/javascripts/promo_login.js
@@ -0,0 +1,6 @@
+function promo_login()
+{
+    document.getElementById("email").value = "veimar.94@gmail.com";
+    document.getElementById("password").value = "password";
+    document.getElementById("log_in").click();
+}
